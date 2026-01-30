3c1ee95a2525cb406a2d68b420c069341467d594
nat <narupe03@gmail.com>
2026-01-30 04:11:38 -0500

    update

---
+++ Actividad/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Actividad
+{
+    //Hay que implementar el método "NumerosPares" que recorra el arreglo "numeros" e imprima solo los números pares.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+           int[] numeros = { 2, 7, 14, 9, 10, 23, 18 };             
+
+            
+        }
+
+        static void NumerosPares()
+        {
+            Console.WriteLine("Numeros pares:");
+
+            for (int i = 0;  i < lista.Length; i++)
+            {
+                if ()
+                {
+                    Console.Write("Es numero par:" + );
+                }
+
+            }
+        }
+    }
+
+}
+
